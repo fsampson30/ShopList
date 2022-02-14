bb5fe98d057b1a4b8c851a304abb3ff5e7cddc2c
fsampson30 <fsampson30@gmail.com>
2022-02-14 17:39:29 -0300

    Initial commit

---
+++ app/src/main/java/com/sampson/shoplist/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sampson.shoplist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sampson.shoplist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
