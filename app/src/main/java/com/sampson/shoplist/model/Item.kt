1e0f29de83b84a816c0deea4376b7b91ecf5c3ed
fsampson30 <fsampson30@gmail.com>
2022-02-15 14:48:45 -0300

    Item.kt changes

--- app/src/main/java/com/sampson/shoplist/model/Item.kt
+++ app/src/main/java/com/sampson/shoplist/model/Item.kt
@@ -1,8 +1,12 @@
 package com.sampson.shoplist.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "table_item")
 data class Item(
-    val id: Int = 0,
-    val name: String = "",
-    val price: String = "",
-    val category: String = ""
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    var name: String = "",
+    var price: String = "",
+    var category: String = ""
 )
