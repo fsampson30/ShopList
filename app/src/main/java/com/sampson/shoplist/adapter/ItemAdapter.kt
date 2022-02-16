6c6820f7ffb9a30c8851244ccf9d47a7974943b6
fsampson30 <fsampson30@gmail.com>
2022-02-16 16:44:24 -0300

    UI features added.

--- app/src/main/java/com/sampson/shoplist/adapter/ItemAdapter.kt
+++ app/src/main/java/com/sampson/shoplist/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.sampson.shoplist.R
@@ -16,6 +17,7 @@ class ItemAdapter (
 
     class ItemViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
         val txtItemName: TextView = itemView.findViewById(R.id.txtItemCardItemName)
+        val imgItemPicture: ImageView = itemView.findViewById(R.id.imgItemCardPicture)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -25,8 +27,24 @@ class ItemAdapter (
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         holder.txtItemName.text = itemsList[position].name
+        holder.imgItemPicture.setImageResource(getImageResource(position))
     }
 
     override fun getItemCount() = itemsList.size
+
+    private fun getImageResource(position: Int): Int {
+        return when (itemsList[position].category) {
+            1 -> R.drawable.meat
+            2 -> R.drawable.cereal
+            3 -> R.drawable.drinks
+            4 -> R.drawable.milk
+            5 -> R.drawable.flour
+            6 -> R.drawable.spices
+            7 -> R.drawable.bread
+            8 -> R.drawable.fish
+            9 -> R.drawable.cleaning
+            else -> R.drawable.others
+        }
+    }
 
 }
