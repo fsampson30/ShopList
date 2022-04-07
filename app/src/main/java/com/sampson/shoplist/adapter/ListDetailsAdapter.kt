bbdb1eee5c718ad3cd1264f4e4ba3877c73a47d2
fsampson30 <fsampson30@gmail.com>
2022-04-07 14:02:45 -0300

    Removed strikethrought textview.

--- app/src/main/java/com/sampson/shoplist/adapter/ListDetailsAdapter.kt
+++ app/src/main/java/com/sampson/shoplist/adapter/ListDetailsAdapter.kt
@@ -24,7 +24,7 @@ class ListDetailsAdapter(
         val imgItem: ImageView = itemView.findViewById(R.id.imgListItemsCardPicture)
         val txtItemName: TextView = itemView.findViewById(R.id.txtListItemsCardItemName)
         val txtItemQuantity: TextView = itemView.findViewById(R.id.txtQttListItemsActivity)
-        val cbbItemDone : CheckBox = itemView.findViewById(R.id.cbbItemDoneListItemsActivity)
+        val cbbItemDone: CheckBox = itemView.findViewById(R.id.cbbItemDoneListItemsActivity)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListDetailsViewHolder {
@@ -33,18 +33,19 @@ class ListDetailsAdapter(
     }
 
     override fun onBindViewHolder(holder: ListDetailsViewHolder, position: Int) {
-        holder.imgItem.setImageResource(ImageResources.getImageResource(itemsList[position].category))
-        holder.txtItemName.text = itemsList[position].item_name
-        holder.txtItemQuantity.text = itemsList[position].quantity.toString()
+        val currentItem = itemsList[position]
+
+        holder.imgItem.setImageResource(ImageResources.getImageResource(currentItem.category))
+        holder.txtItemName.text = currentItem.item_name
+        holder.txtItemQuantity.text = currentItem.quantity.toString()
 
         holder.cbbItemDone.setOnCheckedChangeListener(null)
-        holder.cbbItemDone.isChecked = itemsList[position].isDone
+        holder.cbbItemDone.isChecked = currentItem.isDone
 
         holder.cbbItemDone.setOnCheckedChangeListener { _, isChecked ->
-            //paintItem(holder.txtItemName, isChecked)
-            //paintItem(holder.txtItemQuantity, isChecked)
-            itemsList[position].isDone = !itemsList[position].isDone
+            currentItem.isDone = !currentItem.isDone
         }
+
     }
 
     override fun getItemCount(): Int = itemsList.size
@@ -52,14 +53,5 @@ class ListDetailsAdapter(
     fun submitList(list: MutableList<ItemsListCategory>) {
         this.itemsList = list
         notifyDataSetChanged()
-    }
-
-    private fun paintItem(tvItemName : TextView, isChecked: Boolean) {
-        if (isChecked)  {
-            tvItemName.paintFlags = (tvItemName.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG)
-        } else {
-            tvItemName.paintFlags = (tvItemName.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv())
-
-        }
     }
 }
